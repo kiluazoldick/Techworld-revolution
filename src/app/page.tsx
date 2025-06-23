1eb548406937bc10bf10694d83c85bf14fd7bcb1
Kilua Zoldick <129429563+kiluazoldick@users.noreply.github.com>
2025-06-23 19:55:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
      
 
-      <Header />
+      
       <main>
         <HeroSection />
         <AboutSection />
